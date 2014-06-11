f29645001ac3d86dd5288ae5daaf9d8daf9b38e0
okdshin <kokuzen@gmail.com>
2014-06-11 18:12:36 +0900

    fix name and place

--- behemoth/big_natural.h
+++ behemoth/big_natural.h
@@ -2,9 +2,9 @@
 #define BEHEMOTH_BIG_NATURAL_H
 //20140530
 #include <behemoth/cstdint.h>
-#include <behemoth/big_natural/traits_and_policy.h>
-#include <behemoth/big_natural/basic_big_natural.h>
+#include <behemoth/big_natural/access.h>
 #include <behemoth/big_natural/operator.h>
+#include <behemoth/big_natural/basic_big_natural.h>
 namespace behemoth {
 	typedef basic_big_natural<std::vector<behemoth::uint_least32_t>, 65536> big_natural;
 }// namespace behemoth
--- behemoth/big_natural/operator.h
+++ behemoth/big_natural/operator.h
@@ -1,58 +1,6 @@
 #ifndef BEHEMOTH_BIG_NATURAL_OPERATOR_H
 #define BEHEMOTH_BIG_NATURAL_OPERATOR_H
 //20140606
-#include <cassert>
-#include <behemoth/big_natural/operator/comparison.h>
-#include <behemoth/big_natural/traits_and_policy.h>
-#include <behemoth/big_natural/utility.h>
-#include <behemoth/mpl.h>
-namespace behemoth {
-	namespace big_naturals {
-		template<typename BigNatural>
-		BigNatural& bitand_assign(BigNatural& lhs, const BigNatural& rhs) {
-			assert(behemoth::big_naturals::is_valid(lhs));
-			assert(behemoth::big_naturals::is_valid(rhs));
-			typename behemoth::big_naturals::iterator_type<BigNatural>::type 
-				l_iter = behemoth::big_naturals::begin(lhs);
-			typename behemoth::big_naturals::const_iterator_type<BigNatural>::type 
-				l_end = behemoth::big_naturals::cend(lhs),
-				r_iter = behemoth::big_naturals::cbegin(rhs),
-				r_end = behemoth::big_naturals::cend(rhs);
-			while(l_iter != l_end) {
-				if(r_iter == r_end) {
-					behemoth::big_naturals::resize(lhs, behemoth::big_naturals::size(rhs));
-					break;
-				}
-				*l_iter &= *r_iter;
-				++l_iter; ++r_iter;
-			}
-			behemoth::big_naturals::cut_zeros(lhs);
-			return lhs;
-		}
-			
-		template<typename BigNatural>
-		BigNatural& bitor_assign(BigNatural& lhs, const BigNatural& rhs) {
-			assert(behemoth::big_naturals::is_valid(lhs));
-			assert(behemoth::big_naturals::is_valid(rhs));
-			typename behemoth::big_naturals::iterator_type<BigNatural>::type 
-				l_iter = behemoth::big_naturals::begin(lhs);
-			typename behemoth::big_naturals::const_iterator_type<BigNatural>::type 
-				l_end = behemoth::big_naturals::cend(lhs),
-				r_iter = behemoth::big_naturals::cbegin(rhs),
-				r_end = behemoth::big_naturals::cend(rhs);
-			while(l_iter != l_end) {
-				if(r_iter == r_end) {
-					return lhs;
-				}
-				*l_iter |= *r_iter;
-				++l_iter; ++r_iter;
-			}
-			behemoth::big_naturals::resize(lhs,
-					behemoth::big_naturals::size(rhs));
-			std::copy(r_iter, r_end, l_iter);
-			return lhs;
-		}
-	}// namespace big_naturals
-}// namespace behemoth
-
+#include <behemoth/big_natural/operator/compare.h>
+#include <behemoth/big_natural/operator/bit_logical.h>
 #endif //BEHEMOTH_BIG_NATURAL_OPERATOR_H
