96d96a0c2d0a84cd674925dbdc165756cfd7bd24
okdshin <kokuzen@gmail.com>
2014-07-02 14:58:58 +0900

    add some test code

--- unit_test/big_natural/big_natural.cpp
+++ unit_test/big_natural/big_natural.cpp
@@ -4,7 +4,7 @@
 
 namespace testbeh {
 	void big_natural_unit_test(){
-		std::cout << "###this is unit test function" << std::endl;
+		using namespace behemoth;
 		std::vector<uint_least32_t> vec;
 		vec.push_back(1);
 		vec.push_back(2);
@@ -12,19 +12,67 @@ namespace testbeh {
 		vec.push_back(0);
 		vec.push_back(0);
 
-		//begin
-		{
-			TESTBEH_ASSERT(vec.front() == *behemoth::big_naturals::begin(vec));
-		}
-		/*
-		std::cout << "back " << *(behemoth::big_naturals::end(vec)-1) << std::endl;
-		std::cout << "cut_zeros" << std::endl;
-		behemoth::big_naturals::cut_zeros(vec);
-		std::cout << "front " << *behemoth::big_naturals::begin(vec) << std::endl;
-		std::cout << "back " << *(behemoth::big_naturals::end(vec)-1) << std::endl;
-		*/
-		//std::cout << "radix " << behemoth::big_naturals::radix<std::vector<uint_least32_t> >::value << std::endl;
+		big_natural bn1(vec);
 
+		// begin
+		{
+			TESTBEH_ASSERT(vec.front() == *big_naturals::begin(bn1));
+		}
+
+		// cbegin
+		{
+			TESTBEH_ASSERT(vec.front() == *big_naturals::cbegin(bn1));
+		}
+
+		// end
+		{
+			TESTBEH_ASSERT(vec.back() == *(--big_naturals::end(bn1)));
+		}
+		
+		// cend
+		{
+			TESTBEH_ASSERT(vec.back() == *(--big_naturals::cend(bn1)));
+		}
+		
+		// rbegin
+		{
+			TESTBEH_ASSERT(vec.back() == *big_naturals::rbegin(bn1));
+		}
+
+		// crbegin
+		{
+			TESTBEH_ASSERT(vec.back() == *big_naturals::crbegin(bn1));
+		}
+
+		// rend
+		{
+			TESTBEH_ASSERT(vec.front() == *(--big_naturals::rend(bn1)));
+		}
+		
+		// crend
+		{
+			TESTBEH_ASSERT(vec.front() == *(--big_naturals::crend(bn1)));
+		}
+
+		// radix_value
+		{
+			TESTBEH_ASSERT(65536 == big_naturals::radix_value<big_natural>::value);
+		}
+		
+		// radix_digit_num
+		{
+			TESTBEH_ASSERT(16 == big_naturals::radix_digit_num<big_natural>::value);
+		}
+		
+		// max_value
+		{
+			TESTBEH_ASSERT(65535 == big_naturals::max_value<big_natural>::value);
+		}
+		
+		// size
+		{
+			TESTBEH_ASSERT(3 == big_naturals::size(bn1));
+		}
 	}
 }// namespace testbeh
 
